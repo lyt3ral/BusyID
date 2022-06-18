e4fe71efecdc752b01270caff56a62f23865d63b
Roshan Jagadish <roshan10.rj@gmail.com>
2022-06-18 20:52:52 +0530

    Logo Added

--- app/src/main/java/com/example/busyid/MainActivity.kt
+++ app/src/main/java/com/example/busyid/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.busyid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv: TextView = findViewById(R.id.Heading)
+        tv.text = "BusyID"
     }
 }
